d5d05fa59d96625777bf5db3d625047073a69ee4
muratcelikjp-dot <muratcelikjp@gmail.com>
2026-07-09 12:49:56 +0900

    Restore Abyss atmosphere home

--- js/app-version.js
+++ js/app-version.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = "v1.1.1-abyss-home";
+  const APP_VERSION = "v1.1.7-abyss-atmosphere";
   const BADGE_ID = "app-version-badge";
 
   function positionBadge(badge) {
