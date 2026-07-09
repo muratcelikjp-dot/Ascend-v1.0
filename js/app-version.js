8c18e042664a3418a781ec4948c17226492024a3
muratcelikjp-dot <muratcelikjp@gmail.com>
2026-07-09 13:12:14 +0900

    Polish quests navigation and attributes

--- js/app-version.js
+++ js/app-version.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = "v1.1.7-abyss-atmosphere";
+  const APP_VERSION = "v1.1.9-quests-nav-polish";
   const BADGE_ID = "app-version-badge";
 
   function positionBadge(badge) {
--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //
 // CACHE_VERSION must be bumped on any meaningful app-shell file change.
 const CACHE_PREFIX = "rpg-app-";
-const CACHE_VERSION = "v24";
+const CACHE_VERSION = "v26";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 
 const PRECACHE_URLS = [
